e6fe0a5e4dcd59f9b95f7206de9d0eb9c785ff10
KaiqueGL18 <kaiquegomeslima13@gmail.com>
2022-11-10 11:12:40 -0300

    first commit

---
+++ function.js
@@ -0,0 +1,16 @@
+const x = 5;
+const y = 7;
+const z = 9;
+
+function digaOla(){
+    console.log('OLá');
+}
+
+function multiplica(a, b, t){
+   return a*b*t;
+}
+
+digaOla();
+const resultado = multiplica(x, y, z);
+
+console.log(resultado);
